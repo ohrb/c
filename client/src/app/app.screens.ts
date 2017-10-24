aa80c97a58d4031864ac546f3580447c10bcf30a
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-24 15:44:09 +0300

    [Studio] Add screen "Login"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,16 +1,15 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
-import { MenuComponent } from "./menu/menu.component";
-
+import { MenuComponent } from './menu/menu.component';
+import { Login_PhonePortrait } from '../pages/Login/PhonePortrait/Login';
 export class Screens {
-
   static declarations: Page[] = [
-    MenuComponent
+    MenuComponent,
+    Login_PhonePortrait
   ];
-
   static mapping = {
+    'Login': {
+      PhonePortrait: Login_PhonePortrait
+    }
   }
-
 }
-
-

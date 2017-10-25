1cca90713dc20de580566780a788e717b610bdf3
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-25 16:15:35 +0300

    [Studio] Delete screen "Login"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,15 +1,9 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
-import { Login_PhonePortrait } from '../pages/Login/PhonePortrait/Login';
 export class Screens {
   static declarations: Page[] = [
-    MenuComponent,
-    Login_PhonePortrait
+    MenuComponent
   ];
-  static mapping = {
-    'Login': {
-      PhonePortrait: Login_PhonePortrait
-    }
-  }
+  static mapping = {}
 }
